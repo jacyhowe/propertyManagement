78696f73dc470d622e3f8e2b7413b54b8f55feb0
jacyhowe <jacyhowe@hotmail.com>
2018-07-09 21:32:13 -0600

    created auth components and headerwrapper component

---
+++ src/components/auth/signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signin extends Component {
+    render() {
+        return (
+            <div className="signin">
+                signin
+            </div>
+        )
+    }
+}
+
+export default Signin;
---
+++ src/components/auth/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    render() {
+        return (
+            <div className="signup">
+                Signup
+            </div>
+        )
+    }
+}    
+export default Signup;
