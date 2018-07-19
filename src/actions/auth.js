d26909d890a584e1e2ccd1a73f9fbc421bf0424d
jacyhowe <jacyhowe@hotmail.com>
2018-07-18 18:54:41 -0600

    built signin functionality.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 
 import { ROOT_URL } from '../config';
+import { AUTHENTICATE_USER } from './types';
 
 export function signUp(fields, success) {
     console.log(fields);
@@ -10,8 +11,29 @@ export function signUp(fields, success) {
             .then(response => {
                 const { token } = response.data;
                 localStorage.setItem('token', token);
-               console.log(response);
+               dispatch({
+                   type: AUTHENTICATE_USER,
+                   payload: response.data
+               })
                success()
+            })
+            .catch(err => {
+                if(err) { console.log(err) }
+            })
+    }
+}
+
+export function signIn(fields, success) {
+    return function(dispatch) {
+        axios.post(`${ROOT_URL}/signIn`, fields)
+            .then(response => {
+                const { token } = response.data;
+                localStorage.setItem('token', token);
+               dispatch({
+                   type: AUTHENTICATE_USER,
+                   payload: response.data
+               })
+                success()
             })
             .catch(err => {
                 if(err) { console.log(err) }
