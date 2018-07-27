9df95f57f4df89e33bb5056bb8706e36b5d68c62
jacyhowe <jacyhowe@hotmail.com>
2018-07-26 20:30:29 -0600

    styled newsletter box component.

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import NewsletterBox from './newsletterBox';
 
 class NewsletterGrid extends Component {
     render() {
         return(
             <div className="newsletter-grid">
+            {/* add button */}
+            <NewsletterBox date={new Date()}/>
+            {/* archive */}
+            {/* content */}
             </div>
         )
     }
