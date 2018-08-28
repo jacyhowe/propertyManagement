15c4d3c8093672877a52cc8a3c850e4f80452679
jacyhowe <jacyhowe@hotmail.com>
2018-08-27 20:07:59 -0600

    JSX for request item.

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+
 import Icon from '../icon';
+import Button from '../button';
 
 class RequestsItem extends Component {
 
     render() {
         return (
             <div className='requests-item'>
-               <Icon icon='fas fa-exclamation-triangle'/>
+                    <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>    
+                    <div className='requests-item__title'>
+                        Broken Door Knob
+                    </div>
+                    <div className='requests-item__tenant-unit'>
+                        Jacy - Unit 1811
+                    </div>  
+                    <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
+                    <div className='requests-item__date'>
+                        06/02/91
+                    </div>
+                    <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('trying to change request item status')}/>                    
             </div>
         )
     }
