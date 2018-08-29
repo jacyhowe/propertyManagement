7915793c3273afcd70bd0cefe010eb5a4eeae301
jacyhowe <jacyhowe@hotmail.com>
2018-08-28 20:12:22 -0600

    cleaned up request item grid

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -10,12 +10,13 @@ class RequestsItem extends Component {
             <div className='requests-item'>
                     <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>    
                     <div className='requests-item__title'>
-                        Broken Door Knob
+                        <div className='requests-item__title__text'>Broken Door Knob</div>
+                    <Icon className='requests-item__title__arrow' icon='fas fa-sort-down'/>
                     </div>
                     <div className='requests-item__tenant-unit'>
                         Jacy - Unit 1811
                     </div>  
-                    <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
+                    
                     <div className='requests-item__date'>
                         06/02/91
                     </div>
