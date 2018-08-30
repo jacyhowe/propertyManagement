5c9f9953b8fb366f14cb3859578156fb034621be
jacyhowe <jacyhowe@hotmail.com>
2018-08-29 21:08:16 -0600

    added hover animation and move button animation/opacity

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -3,15 +3,36 @@ import React, { Component } from 'react';
 import Icon from '../icon';
 import Button from '../button';
 
+import AnimateHeight from 'react-animate-height';
+
 class RequestsItem extends Component {
+
+    constructor() {
+        super()
+        
+        this.state = {
+            height: 0
+        }
+    }
+
+    toggleDropDown = () => {
+        var element = document.getElementById('requests-item');
+        if(this.state.height == 0) {
+            element.classList.add('bg-F8');
+            this.setState({height: 'auto'})            
+        } else {
+            element.classList.remove('bg-F8');
+            this.setState({height: 0})
+        }
+    }
 
     render() {
         return (
-            <div className='requests-item'>
+            <div id='requests-item' className='requests-item'>
                     <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>    
                     <div className='requests-item__title'>
                         <div className='requests-item__title__text'>Broken Door Knob</div>
-                    <Icon className='requests-item__title__arrow' icon='fas fa-sort-down'/>
+                    <Icon callback={() => this.toggleDropDown()} className='requests-item__title__arrow' icon='fas fa-sort-down'/>
                     </div>
                     <div className='requests-item__tenant-unit'>
                         Jacy - Unit 1811
@@ -22,16 +43,24 @@ class RequestsItem extends Component {
                     </div>
                     <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('trying to change request item status')}/> 
                     <div className='requests-item__description'>
-                        <img 
-                            className='requests-item-description-image'
-                            src='http://via.placeholder.com/160x94'                            
-                        />
-                        <p className='requests-item__description-text'>
-                            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                        </p>
-                    </div>                   
+                        <AnimateHeight
+                            duration={300}
+                            height={this.state.height}
+                        >
+                            <div className='item-description'>
+                                <img 
+                                    className='item-description-img'
+                                    src='http://via.placeholder.com/160x94'                            
+                                />
+                                <p className='item-description-text'>
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                                </p>
+                            </div>
+                        </AnimateHeight>  
+                    </div>            
+
             </div>
         )
     }
