31f37c4d22b2e4ae75cdc788a8d6306a15b9851f
jacyhowe <jacyhowe@hotmail.com>
2018-09-10 19:12:33 -0600

    put correct item count in boex for requests feature

--- src/components/requests/requestBoxes.js
+++ src/components/requests/requestBoxes.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import RequestsBox from './requestsBox';
+import { request } from 'http';
+
 
 class RequestBoxes extends Component {
     render() {
         return (
             <div className='request-boxes'>
-                 <RequestsBox title='Pending' count={9}/>
-                <RequestsBox title='In-progess' count={3}/>
-                <RequestsBox title='Complete' count={5}/>
+                 <RequestsBox title='Pending' count={this.props.pendingCount}/>
+                <RequestsBox title='In-progess' count={this.props.progressCount}/>
+                <RequestsBox title='Complete' count={this.props.completeCount}/>
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    const { requests } = state.requests;
+
+    var pendingCount = 0;
+    var progressCount = 0;
+    var completeCount = 0;
+
+        requests.map(request => {
+            if(request.status == 'pending') {
+                pendingCount += 1;
+            } else if(request.status == 'progress') {
+                progressCount += 1;
+            } else if(request.status == 'complete') {
+                completeCount += 1;
+            }
+        })
+        return {
+            pendingCount,
+            progressCount,
+            completeCount
+    }
+}
+
+RequestBoxes = connect(mapStateToProps)(RequestBoxes);
 
 export default RequestBoxes;
